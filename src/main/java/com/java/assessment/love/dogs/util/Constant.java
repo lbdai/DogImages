6652c6f851a37bbcef090de12380c8c852f87f3d
Dai Le <dai.lebao@gmail.com>
2025-06-05 22:40:29 +0700

    separate RestTemplate to multipart/form-data and application/json type

--- src/main/java/com/java/assessment/love/dogs/util/Constant.java
+++ src/main/java/com/java/assessment/love/dogs/util/Constant.java
@@ -2,6 +2,6 @@ package com.java.assessment.love.dogs.util;
 
 public class Constant {
   public static final String API_HEADER = "x-api-key";
-  public static final String CONTENT_TYPE_JSON = "Content-Type";
+  public static final String CONTENT_TYPE_JSON = "application/json";
   public static final String CONTENT_TYPE_FILE = "multipart/form-data";
 }
